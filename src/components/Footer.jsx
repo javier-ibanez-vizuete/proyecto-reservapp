066d9b2e282850cfd98f19e6cfb54f088fcb30fb
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-09-18 13:07:57 +0200

    feat(Footer): Add Theme Styles for Footer Background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { LanguageContext } from "../contexts/LanguageContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 import { Container } from "./Container";
 import { ThemeButton } from "./UI/ThemeButton";
 
 export const Footer = () => {
     const { lang, languages, handleLang } = useContext(LanguageContext);
     const { user } = useContext(AuthContext);
+    const { theme } = useContext(ThemeContext);
 
     return (
-        <footer className="flex flex-col py-6">
+        <footer
+            className={`flex flex-col py-6 ${
+                theme === "light" ? "bg-accent-background" : "bg-accent-background-dark"
+            }`}
+        >
             <Container className="gap-2">
                 <div className="flex justify-between items-center">
                     {!user && (
