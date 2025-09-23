591f75b81e35b9aebb3d99523fbf020815c81d0f
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-09-23 19:42:58 +0200

    fix(App): vite.config

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,19 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import { App } from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { LanguageProvider } from "./contexts/LanguageContext.jsx";
+import { App } from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { LanguageProvider } from "./contexts/LanguageContext.jsx";
 import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <BrowserRouter>
-            <LanguageProvider>
-                <AuthProvider>
-                    <ThemeProvider>
-                        <App />
-                    </ThemeProvider>
-                </AuthProvider>
-            </LanguageProvider>
-        </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter>
+        <LanguageProvider>
+            <AuthProvider>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </AuthProvider>
+        </LanguageProvider>
+    </BrowserRouter>
 );
